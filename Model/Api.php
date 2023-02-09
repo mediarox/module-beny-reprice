d80ea9e04174e3cfecb787da3375f3611a34953e
Marcus Bernt <berntmarcus86@gmail.com>
2023-02-09 13:41:51 +0100

    [!] fixed read time out > set new timeout limit for curl

--- Model/Api.php
+++ Model/Api.php
@@ -148,6 +148,7 @@ class Api
             'adapter'     => Curl::class,
             'curloptions' => [
                 CURLOPT_RETURNTRANSFER => 1,
+                CURLOPT_TIMEOUT => 120
             ],
         ]);
 
